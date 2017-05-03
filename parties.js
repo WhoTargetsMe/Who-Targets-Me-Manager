40abf7093d62f182b16edd89ad776dc9527d1a51
Jan Baykara <jan@baykara.co.uk>
2017-05-03 19:26:00 +0100

    Funky quick-click icons for suggest/select

--- parties.js
+++ parties.js
@@ -19,13 +19,51 @@ $(document).ready(function() {
 		$("#app").show();
 		console.log("All loaded");
 
+		Vue.component('party-span', {
+			template: "#party-span",
+			props: ['party'],
+			methods: {
+				invertColor: function(hex, bw) {
+				    if (hex.indexOf('#') === 0) {
+				        hex = hex.slice(1);
+				    }
+				    // convert 3-digit hex to 6-digits.
+				    if (hex.length === 3) {
+				        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+				    }
+				    if (hex.length !== 6) {
+				        throw new Error('Invalid HEX color.');
+				    }
+				    var r = parseInt(hex.slice(0, 2), 16),
+				        g = parseInt(hex.slice(2, 4), 16),
+				        b = parseInt(hex.slice(4, 6), 16);
+				    if (bw) {
+				        // http://stackoverflow.com/a/3943023/112731
+				        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+				            ? '#000000'
+				            : '#FFFFFF';
+				    }
+				    // invert color components
+				    r = (255 - r).toString(16);
+				    g = (255 - g).toString(16);
+				    b = (255 - b).toString(16);
+				    // pad each with zeros and return
+				    return "#" + padZero(r) + padZero(g) + padZero(b);
+				}
+			}
+		});
+
 		Vue.component('advertiser-table', {
 			template: "#advertiser-table",
 			props: ['data','parties','politicians', 'suggestengine'],
 			methods: {
+				topparties: function(x) {
+					return this.parties[1].list.slice(0,x);
+				},
 				suggestParty: function(advertiser) {
 					// console.log("Suggesting for "+advertiser.advertiser+", with "+this.politicians.length+" possible matches")
-					var likelyParties = []
+					var App = this;
+					var likelyParties = new Set();
 
 					if(this.suggestengine) {
 						var matchedEntities = this.politicians.filter(function(candidate) {
@@ -49,11 +87,13 @@ $(document).ready(function() {
 						if(matchedEntities.length && matchedEntities.length > 0) {
 							// console.log("Matches for "+advertiser.advertiser,matchedEntities)
 							matchedEntities.forEach(function(entity) {
-								likelyParties.push(entity.party);
+								likelyParties.add(entity.party);
 							});
 						}
 					}
 
+					likelyParties = Array.from(likelyParties)
+					likelyParties = likelyParties.map((id) => App.parties[1].list.find((someParty)=> someParty.id == id) );
 					return likelyParties;
 				}
 			}
@@ -89,7 +129,7 @@ $(document).ready(function() {
 						// Sanitise newArray. For any element with political == 'false' or '', clear element affiliation
 						App.advertisers.forEach(function(advertiser,index) {
 							if(advertiser.political == 'false' || advertiser.political == '') {
-								// console.log("Clearing affil for "+advertiser.advertiser)
+								console.log("Clearing affil for "+advertiser.advertiser)
 								App.advertisers[index].affiliation = '';
 							}
 						})
